968ac9ce16aa43e11210a89678e7bf80d4e8bffd
Dusan Matejka <dusky@dusans-imac.local>
2015-10-17 16:04:26 +0200

    Initial Commit

---
+++ hra55/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hra55
+//
+//  Created by Dusan Matejka on 10/17/15.
+//  Copyright © 2015 Dusan Matejka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
